127a67b35528a770210495fcbe5c6d6e5e44c572
knoc-off <github@knoc.one>
2023-12-28 10:30:54 +0100

    fixed bug, gonna stop working on this now

--- runme.sh
+++ runme.sh
@@ -1,25 +1,17 @@
-#/usr/bin/env nix-shell
-#nix-shell -i bash -p jq qrencode
+# /usr/bin/env bash
+# /usr/bin/env nix-shell -i bash -p jq qrencode
 
-#/usr/bin/env bash
+# on fail exit
+set -e
 
 function hit() {
-
     ran=$((RANDOM%$MAXRANGE)) # needs tweaking.
     ran=$(printf "%05d" $ran)
-
-    if [ -f numbers.txt ]
-    then
-        echo "numbers.txt exists"
-    else
-        echo "numbers.txt does not exist"
-        touch numbers.txt
-    fi
 
     # check if the number is already in use
     if grep -Fxq "$ran" numbers.txt
     then
-        echo "Number already in use, try again"
+        #echo "Number already in use, try again"
         hit
         return
     fi
@@ -33,6 +25,15 @@ function hit() {
 }
 
 function init() {
+
+    if [ -f numbers.txt ]
+    then
+        echo "numbers.txt exists"
+    else
+        echo "numbers.txt does not exist"
+        touch numbers.txt
+    fi
+
     # check if environment is set
     if [ -z "$HITSTER_COUNTRYCODE" ]
     then
@@ -41,32 +42,42 @@ function init() {
         #jq -r '.countryCode | keys[]' countries.json
         # print out the country codes and an index next to them
         for i in "${!codes[@]}"; do
-            echo "$i ${codes[$i]}"
+            # format the index to be 2 spaces wide
+            printf "%02d: %s\n" $i "${codes[$i]}"
+
         done
 
         # ask for input
-        read -p "Enter a number: " input
+        read -p "Enter a number/country code: " input
 
         # check if input is a number
-        if ! [[ "$input" =~ ^[0-9]+$ ]]
+        if [[ "$input" =~ ^[0-9]+$ ]]
         then
-            echo "Not a number"
-            hit
-            return
+            export HITSTER_COUNTRYCODE=${codes[10#$input]}
+        elif [[ " ${codes[@]} " =~ " ${input} " ]] then
+            export HITSTER_COUNTRYCODE="$input"
+        else
+            echo "Invalid input"
+            exit 1
         fi
 
-        export HITSTER_COUNTRYCODE=${codes[$input]}
     fi
 
 
     # based on the country code,
     URL=$(jq -r ".countryCode.$HITSTER_COUNTRYCODE.url" countries.json)
     MAXRANGE=$(jq -r ".countryCode.$HITSTER_COUNTRYCODE.max" countries.json)
+    printf "MAXRANGE: %s\n" $MAXRANGE
 }
 
 
 init
 
-# loop forever wait for read
-while true; do hit; read -n1; done
+
+while true;
+do
+    hit;
+    # if the input is i (invalid) then use jq to set the max number to the current numbe
+    read -n1;
+done
 
